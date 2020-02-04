773c3cb4bb4149b75820c6a29f6af6f919ec61d2
Vladislav Mosin <vmos1999@mail.ru>
2020-02-04 18:28:44 +0300

    hopefully last fix

--- src/test/java/com/hse/cli/CommandLauncherTest.java
+++ src/test/java/com/hse/cli/CommandLauncherTest.java
@@ -204,7 +204,7 @@ class CommandLauncherTest {
 
     @Test
     void externalFunction() throws ExternalFunctionRuntimeException, ParsingException, VariableNotInScopeException, IOException {
-        launcher.launch("cd Test");
+        launcher.launch("ls");
     }
 
     @Test
